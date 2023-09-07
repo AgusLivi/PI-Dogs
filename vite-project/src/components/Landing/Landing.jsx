ec4ef8a22f7661d0a9519fc3cd71d476ad37dc69
AgustinLiviñi <agustinlivini07@gmail.com>
2023-09-07 18:43:38 -0300

    pi

---
+++ vite-project/src/components/Landing/Landing.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Landin = ()=>{
+    const homeButton = ()=>{
+
+    }
+
+
+    return(
+        <div>
+            <h1>Dogs</h1>}
+            <button onClick={}>Home</button>
+        </div>
+    )
+}
